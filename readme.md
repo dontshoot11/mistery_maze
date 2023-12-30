e2c0ce5d47be01267d9add6e8c5359389b110644
Artem Nikitin <a.nikitin@dataduck.com>
2023-12-30 01:58:13 +0100

    add file import

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 pure js 3d videogame
 
 kudos for engine and insiration to https://twitter.com/ivanov_dev
+
+https://dontshoot11.github.io/mystery_maze/
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,18 @@ const config = {
 				test: /\.css$/,
 				use: ["style-loader", "css-loader"],
 			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "images/", // Папка, куда будут скопированы изображения
+						},
+					},
+				],
+			},
 		],
 	},
 };
